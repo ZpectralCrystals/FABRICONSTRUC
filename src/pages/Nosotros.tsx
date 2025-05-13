ffda63f79bbd0641ae686da10efbee587928d004
Gustavo Galvez Delgado <59744063+ZpectralCrystals@users.noreply.github.com>
2025-05-13 00:46:24 -0500

    updated files

--- src/pages/Nosotros.tsx
+++ src/pages/Nosotros.tsx
@@ -14,7 +14,7 @@ const Nosotros = () => {
         <div className="grid md:grid-cols-2 gap-10 mt-10">
           <div>
             <h3 className="text-2xl font-semibold mb-3 text-gray-700">Misión</h3>
-            <p className="text-gray-600">
+            <p className="text-gray-600 text-justify">
               Consolidarnos como una empresa reconocida a nivel nacional por la calidad, seguridad y
               eficiencia de nuestros servicios en infraestructura industrial y civil, respondiendo a
               los retos de nuestros clientes con profesionalismo.
@@ -23,7 +23,7 @@ const Nosotros = () => {
 
           <div>
             <h3 className="text-2xl font-semibold mb-3 text-gray-700">Visión</h3>
-            <p className="text-gray-600">
+            <p className="text-gray-600 text-justify" >
               Ser una empresa líder en el sector, destacando por la excelencia operativa,
               innovación constante y compromiso con el desarrollo sostenible y el bienestar de
               nuestros colaboradores.
@@ -33,20 +33,43 @@ const Nosotros = () => {
 
         <div className="mt-12">
           <h3 className="text-2xl font-semibold mb-3 text-gray-700">Política de Calidad</h3>
-          <p className="text-gray-600">
-            En FABRICONSTRUC, asumimos el compromiso de ofrecer servicios con altos estándares de
-            calidad y seguridad, contando con personal calificado y fomentando una cultura de mejora
-            continua para la satisfacción de nuestros clientes.
+          <p className="text-gray-600 text-justify">
+            FABRICONSTRUC INDUSTRIALES asume el compromiso de brindar a nuestros clientes servicios
+            de Calidad en términos de seguridad y trabajo terminado, con estándares de eficacia y eficiencia.
+            Contando para ello con personal calificado y una cultura de calidad basado en la Mejora Continua.
           </p>
+          <p className="text-gray-600 text-justify ">FABRICONSTRUC INDUSTRIALES cumple con los estándares sobre Gestión de Calidad que nos aplica.
+            Nuestra motivación de mejorar siempre es la satisfacción de nuestros clientes al brindarles
+            calidad en nuestros servicios.</p>
+          <p className="text-gray-600 text-justify">Nuestro Sistema de Gestión de Calidad está basado en el compromiso de todos los miembros de nuestra Organización.</p>
         </div>
 
         <div className="mt-6">
           <h3 className="text-2xl font-semibold mb-3 text-gray-700">Política de Seguridad</h3>
-          <p className="text-gray-600">
-            Mantenemos un ambiente de trabajo seguro, implementando medidas preventivas para
-            proteger la integridad física y la salud de nuestros trabajadores, así como revisiones
-            y auditorías continuas en nuestros sistemas de gestión.
+          <p className="text-gray-600 text-justify">
+            Implementar controles operacionales eficaces en el
+            marco de la prevención de todo incidente que pudiera
+            afectar a las personas que trabajan bajo nuestra
+            responsabilidad además mantener programas de
+            capacitación y entrenamiento continuo.
           </p>
+          <p className="text-gray-600 text-justify">
+            Brindar un ambiente de trabajo seguro y saludable
+            adecuados para prevenir accidentes y lesiones a la
+            salud que se originen durante el trabajo.
+          </p>
+          <p className="text-gray-600 text-justify">
+            Mantener a disposición de las partes interesadas
+            nuestra política, así como el avance en nuestro
+            desempeño en seguridad.
+          </p>
+          <p className="text-gray-600 text-justify">
+            Llevar a cabo revisiones y auditorías de nuestro
+            sistema de gestión de seguridad y salud ocupacional
+            para verificar y evaluar el cumplimiento de los objetivos
+            y metas de seguridad.
+          </p>
+
         </div>
       </div>
     </section>
